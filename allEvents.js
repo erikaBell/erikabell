fe420c48cd5db4031204702f2d073683b0607ef3
erikaBell <67438372+erikaBelll@users.noreply.github.com>
2021-04-17 11:43:24 -0700

    fixed rendering issues

--- allEvents.js
+++ allEvents.js
@@ -1,179 +1,136 @@
+// googleSheet data of events - future and past
 var public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSOnp2BPxtloqa6zrYoyjiS9fJY0Lqq6Bc4yVaUSBdGONBsjZSQXgaScGwdLqhzcYMeS30x_CqHYTp_/pub?gid=435615926&single=true&output=csv';
 
-      function init() {
-        Papa.parse(public_spreadsheet_url, {
-          download: true,
-          header: true,
-          complete: showInfo
-        })
-      }
+function init() {
+  Papa.parse(public_spreadsheet_url, {
+    download: true,
+    header: true,
+    complete: showInfo
+  })
+}
 
-    window.addEventListener('DOMContentLoaded', init)
-      
-    function showInfo(results) {
-      var archiveBlock = document.getElementById('list-container');
-      const data = results.data
-      
-	  const allYears = data.map(data => data.year)
-	  
-      const uniqueYears = allYears.filter(function(item, index){
-         return allYears.indexOf(item) >= index;
-     });
+window.addEventListener('DOMContentLoaded', init)
 
-  // CHOOSE YEAR BASED OFF INDEX OF ITEM IN UNIQURE YEARS VARIABLE
-       const allData = data.map(data => {
-         if (data.year === uniqueYears[4]) {
-          return (
-            `<div class="past-performances"> 
-              <div class="datesAndLocations"> 
-                <p class="dates"> ${data.date}</p> 
-                <p class="locations"> ${data.location}</p>
-              </div>
-              <div class="piecesAndDescripts">
-                <p class="pieces">
-                  ${data.type === 'Composition' ? data.premiere + " "+`<i>${data.piece}</i>` : data.piece}
-                </p>
-                <p class="descriptions"> ${data.description}</p>
-              </div>
-            <br>
-          </div>`
-            )
-         }
-       })
+/**
+* 'showInfo' is connected to papaparse.js (code from GitHub), showInfo is acting as 'main'
+* @param {object} data - public_spreadsheet_url 
+*/
+function showInfo(results) {
+  const data = results.data
+  const allYearsArr = []
+  const dataMap = data.map(d => {allYearsArr.push(d.year)})
+  const eachYear = new Set(allYearsArr)
 
-      document.getElementById("list-container").innerHTML = allData.join(" ")
+  updateUI(data)
+}
 
-      }
-
-
-// // TODO: refactor one JS page maintains all performances 
-
-// const public_spreadsheet_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSOnp2BPxtloqa6zrYoyjiS9fJY0Lqq6Bc4yVaUSBdGONBsjZSQXgaScGwdLqhzcYMeS30x_CqHYTp_/pub?gid=435615926&single=true&output=csv';
-
-// // Related to papaParse.js, code from github repo
-//   function init() {
-//     Papa.parse(public_spreadsheet_url, {
-//       download: true,
-//       header: true,
-//       complete: showInfo
-//     })
-//   }
-//   window.addEventListener('DOMContentLoaded', init)
-
-// function renderEvents(results) {
-//   const data = results.data
-  
-
-//   document.getElementById("list-container").innerHTML = allData.join(" ")
-// }
-
-// //TODO: Years will become buttons, this is for the functions to know which data to render
-//   button.addEventListener('click', function() {
-    
-//   })
-
-// //TODO: make the 'clicked' years at top automated, so i don't need to add on each year as time passes.    Make Year in background (pink big font) also automated to change
-// /**
-//  * Renders events onto the DOM
-//  * @param {Object} eventsData [all events retrieved from google doc]
-//  * @return {HTML}
-//  */
-// function renderPerformances(data){
-//   if (data.year === data.year) {
-//   return (
-//     `<div class="past-performances"> 
-//       <div class="datesAndLocations"> 
-//         <p class="dates"> ${data.date}</p> 
-//         <p class="locations"> ${data.location}</p>
-//       </div>
-//       <div class="piecesAndDescripts">
-//         <p class="pieces">
-//           ${data.type === 'Composition' ? data.premiere + " "+`<i>${data.piece}</i>` : data.piece}
-//         </p>
-//         <p class="descriptions"> ${data.description}</p>
-//       </div>
-//     <br>
-//   </div>`
-//     )
-//   }
-// }
+//TODO: figure out how to make initial render of _most recent year_ and then all othe
+/**
+* The updateUI function updates based on which year is clicked in yearNav
+* @param {array of objects} data - based on year
+* @return {html} renders onto DOM
+*/
+function updateUI(data){
+  const eachYear = data.map(data => {
+    if (data.year === '2021') {
+      return (
+        `<div class="past-performances"> 
+          <div class="datesAndLocations"> 
+            <p class="dates"> ${data.date}</p> 
+            <p class="locations"> ${data.location}</p>
+          </div>
+          <div class="piecesAndDescripts">
+            <p class="pieces">
+              ${data.type === 'Composition' ? data.premiere + " "+`<i>${data.piece}</i>` : data.piece}
+            </p>
+            <p class="descriptions"> ${data.description}</p>
+          </div>
+        <br>
+        </div>`
+      )
+    }
+  })
+  document.getElementById("list-container").innerHTML = eachYear.join(" ")
+}
 
 
+//TODO: Years will become buttons, this is for the functions to know which data to render
+//TODO: make the 'clicked' years at top automated, so i don't need to add on each year as time passes.    Make Year in background (pink big font) also automated to change
 
-// function buttonClick(){
-
-// }
-
-// function yearNavigation(year){
-// 	year.forEach(y => {
-// 	`<li><button type="button" onClick="${buttonClick()}">${y}</button></li>`
-// }
-// }
-
-// // Removed performances na JS and pasted into allEvents js 
-// const eventsData = require('./allEvents.js')
-
-// const { data } = eventsData.data
-// console.log(data)
-// const performancesNavTemplate = document.createElement('template')
-// performancesNavTemplate.innerHTML = `
-// 	<style>
-// 		* {
-// 			margin: 0;
-// 			padding: 0;
-// 			box-sizing: border-box;
-// 		}
-// 		ul{
-// 			margin-bottom: 8em;
-// 			font-weight: bold;
-// 			list-style-type: none;
-// 			display: flex;
-// 			justify-content: space-around;
-// 		}
-// 		a {
-// 			text-decoration: none;
-// 			color: #333333;
-			
-// 		}
-// 		a:hover {
-// 			font-size: 2rem;
-// 		}
-// 		.performance-container {
-// 			position: fixed;
-// 			display: inline;
-// 			left: 2em;
-// 			z-index: -10
-// 		}
-// 		.performances-title {
-// 			color: rgba(255, 255, 255, 0.49);
-// 			-webkit-text-stroke-width: 1px;
-//  			-webkit-text-stroke-color: #333333;
-// 			font-size: 15rem;
-// 			overflow: hidden;
-// 		}
-// 		::slotted(span){
-// 			display: inline;
-// 			position: relative;
-// 			font-size: 6em;
-// 			color: pink;
-// 			-webkit-text-stroke-width: 1px;
-//  			-webkit-text-stroke-color: #333333;
-// 		}
-// 	</style>
-// 		<div class='performance-container'>
-// 			<h1 class="performances-title">Events</h1>
-// 			<h3 id="year">
-// 			 <slot name="performance-year">default</slot>
-// 			</h3>
-// 		</div>
+// Attribute Button Year === Year in data, render information to DOM
+function yearOnClick(e, year){
+  if(e.getAttribute('data-year') === year){
+      renderEvents()
+  }
+}
+/**
+ * @param {object} years
+ * @return
+ */
+function yearNavigation(eachYear){
+  for (year of eachYear){
+     `<li><button type="button" data-year="${year}" onClick="${yearOnClick(e, year)}">${year}</button></li>`
+  }
+}
 	
-// 	  <ul>
-//        ${yearNavigation(data.year)}
-//       </ul>
-// `
+
+const performancesNavTemplate = document.createElement('template')
+performancesNavTemplate.innerHTML = `
+	<style>
+		* {
+			margin: 0;
+			padding: 0;
+			box-sizing: border-box;
+		}
+		ul{
+			margin-bottom: 8em;
+			font-weight: bold;
+			list-style-type: none;
+			display: flex;
+			justify-content: space-around;
+		}
+		a {
+			text-decoration: none;
+			color: #333333;
+			
+		}
+		a:hover {
+			font-size: 2rem;
+		}
+		.performance-container {
+			position: fixed;
+			display: inline;
+			left: 2em;
+			z-index: -10
+		}
+		.performances-title {
+			color: rgba(255, 255, 255, 0.49);
+			-webkit-text-stroke-width: 1px;
+ 			-webkit-text-stroke-color: #333333;
+			font-size: 15rem;
+			overflow: hidden;
+		}
+		::slotted(span){
+			display: inline;
+			position: relative;
+			font-size: 6em;
+			color: pink;
+			-webkit-text-stroke-width: 1px;
+ 			-webkit-text-stroke-color: #333333;
+		}
+	</style>
+		<div class='performance-container'>
+			<h1 class="performances-title">Events</h1>
+			<h3 id="year">
+			 <slot name="performance-year">default</slot>
+			</h3>
+		</div>
+	
+	  <ul>
+       placeholder
+    </ul>
+`
 // //TODO: for each DATE ... create: button ... 
-
-
 
 class PerformancesNav extends HTMLElement {
 	constructor(){
